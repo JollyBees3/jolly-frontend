92d611b763b971034284f335ad5546cd8c04bd1e
Benedicta Davour <benedictadavour777@gmail.com>
2024-10-08 13:55:25 -0700

    worked on update page

--- src/pages/addBook/index.jsx
+++ src/pages/addBook/index.jsx
@@ -8,7 +8,7 @@ const AddBook = () => {
             {/* Page Content */}
             <div className="h-[100vh] flex">
                 <div className="w-[40%] h-[100%] ">
-                    img goes here
+                    <div className="bg-[grey] w-[50%] h-[60%] mt-[15%] ml-[20%]"></div>
                 </div>
                 <div className="w-[60%] ">
                     <form className="w-[90%] h-[90%] mt-[10%] border rounded-sm shadow-xl px-[1em]">
@@ -31,7 +31,7 @@ const AddBook = () => {
                         <div className="flex flex-col mb-[0.5em] h-[40%] px-[2em] mt-[1em]">
                             <label htmlFor="bookTitle" className="text-[1.3em]">Description</label>
                             <input id="bookTitle" type="text" className="w-[80%] h-[60%] border" p required />
-                            <Link to="" className="mb-[1em] w-[40%] font-bold text-[1.5em] p-[0.3em] bg-[#000080] text-center text-[white] rounded-sm">Submit Story</Link>
+                            <Link to="" className="mb-[1em] w-[40%] font-bold text-[1.5em] p-[0.3em] bg-[#000080] text-center text-[white] rounded-sm mt-[1em]">Submit Story</Link>
                         </div>
                        
                         
