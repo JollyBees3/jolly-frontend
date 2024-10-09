4f9dcc3f90d33ac5a22278c8783cc03d7599d064
Benedicta Davour <benedictadavour777@gmail.com>
2024-10-09 10:29:40 +0000

    space

--- src/pages/bookDetails/index.jsx
+++ src/pages/bookDetails/index.jsx
@@ -6,7 +6,8 @@ const BookDetails = () => {
       
           <h3 className="ml-[35%] text-[1.5em] font-bold mt-[0.7em]">Lorem ipsum </h3>
           <p className="ml-[35%] mt-[0.7em] w-[60%]">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Blanditiis tempore laboriosam rem, aperiam, temporibus exercitationem vel ipsa voluptatum minima explicabo, nihi</p>
-          <p className="ml-[35%] mt-[0.7em] text-[#000080] font-medium">Read more</p>
+          <p className="ml-[35%] mt-[0.7em] text-[#000080] font-medium">Read more </p>
+          <p className="ml-[35%] mt-[0.7em] text-[#000080] font-medium">Read more </p>
           <p className="ml-[35%] mt-[3em] font-semibold">tempore laboriosam rem, aperiam, temporibus exercitationem</p>
       </div>
       <div className="h-[40%] w-[15%] shadow-md border mt-[-33%] ml-[20%] z-20 bg-[white]">
